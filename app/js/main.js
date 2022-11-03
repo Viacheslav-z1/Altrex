272a722d571942bc1c038d56af4315486a72c2a2
slavik <slavik.zaliotnyi@gmail.com>
2022-11-03 15:38:18 +0200

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,6 @@
 $(function () {
-
-
   const anchors = document.querySelectorAll('a[href*="#"]')
-
+  
   for (let anchor of anchors) {
     anchor.addEventListener('click', function (e) {
       e.preventDefault()
@@ -15,4 +13,29 @@ $(function () {
       })
     })
   }
+
+  const menuBtn = document.querySelector('.header__btn'),
+    menuList = document.querySelector('.menu__list'),
+    body = document.querySelector('.body'),
+    menuItem = document.querySelectorAll('.menu__item');
+
+  menuBtn.addEventListener('click', function (e) {
+    e.preventDefault;
+    if (menuList.classList.contains('open')) {
+      menuList.classList.remove('open');
+      menuBtn.classList.remove('active');
+      body.classList.remove('lock');
+    } else {
+      menuList.classList.add('open');
+      menuBtn.classList.add('active');
+      body.classList.add('lock');
+    }
+  });
+  menuItem.forEach(item => {
+    item.addEventListener('click', function () {
+      menuList.classList.remove('open');
+      menuBtn.classList.remove('active');
+      body.classList.remove('lock');
+    })
+  });
 })
